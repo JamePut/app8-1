34331b2576698ed63200c6d763e643938fb74af4
JamePut <110074605+JamePut@users.noreply.github.com>
2023-10-13 15:58:29 +0700

    create form-post

--- local/src/App.js
+++ local/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 //import RestBasic from './rest-basic'
-import FormGet from './form-get'
+//import FormGet from './form-get'
+import FormPost from './form-post'
 export default function App() {
-  return <FormGet/>
+  return <FormPost/>
 }
